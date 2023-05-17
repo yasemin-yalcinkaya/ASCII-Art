6c352c0251ddefda237e91fc1507529dd47bca18
Emre Kara <emre.kara11@ogr.sakarya.edu.tr>
2023-05-17 17:43:21 +0300

    first 14 num used for file size and ofset calculations in raw2array class, results printed to console

--- include/Raw2Array.hpp
+++ include/Raw2Array.hpp
@@ -1,9 +1,10 @@
 #ifndef Raw2Array_hpp
 #define Raw2Array_hpp
 #include <string>
-class Raw2Array {
-	public:
-		Raw2Array(std::string rawData);
-		~Raw2Array();
+class Raw2Array
+{
+public:
+	Raw2Array(std::string rawData);
+	~Raw2Array();
 };
-#endif 
+#endif
